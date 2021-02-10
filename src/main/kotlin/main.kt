5c8333b5ce313a90ea7e7768c3eff4ff8b0cd998
Timur Zaripov <zaripov_t99@mail.ru>
2021-02-10 11:14:21 +0700

    Music-lover.v2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,34 +2,44 @@ import java.util.*
 
 fun main() {
 
-    val lastBuy = 1001
-    val scanner = Scanner(System.`in`)
+    var lastBuy = 1001.0
+    var scanner = Scanner(System.`in`)
     var regularClient = false
-    val sale = 0
+    var sale = 0.0
 
     while (true) {
         println("Введи сумму покупки или напиши -1 для выхода")
-        val buy = scanner.nextInt()
+        var buy = scanner.nextDouble()
         if (regularClient == true) {
-            val sale = buy * 0.01
+            sale = buy * 0.01
         }
-        if (buy == -1) {
+        if (buy == -1.0) {
+
             break
-        } else if (lastBuy <= 1000) {
-            val lastBuy = buy - sale
-            println("Скидки нет\n" +
-                    "Сумма покупки составила: ${lastBuy} руб")
-        } else if ((lastBuy > 1000) && (lastBuy <= 10000)) {
-            val lastBuy = (buy - 100) - sale
-            println("Скидка 100руб\n" +
-                    "Сумма покупки составила: ${lastBuy} руб")
-        } else if (lastBuy > 10000) {
-            val lastBuy = (buy - buy * 0.05) - sale
-            println("Скидка 5% от суммы покупки\n" +
-                    "Сумма покупки составила: ${lastBuy}")
+        } else if (lastBuy <= 1000.0) {
+
+            lastBuy = buy - sale
+            println(
+                "Скидки нет\n" +
+                        "Сумма покупки составила: $lastBuy руб"
+            )
+        } else if ((lastBuy > 1000.0) && (lastBuy <= 10000.0)) {
+
+            lastBuy = (buy - 100.0) - sale
+            println(
+                "Скидка 100руб\n" +
+                        "Сумма покупки составила: $lastBuy руб"
+            )
+        } else if (lastBuy > 10000.0) {
+
+            lastBuy = (buy - buy * 0.05) - sale
+            println(
+                "Скидка 5% от суммы покупки\n" +
+                        "Сумма покупки составила: $lastBuy"
+            )
         }
 
-        if (buy != 0) {
+        if (buy != 0.0) {
             regularClient = true
         } else {
             regularClient = false
