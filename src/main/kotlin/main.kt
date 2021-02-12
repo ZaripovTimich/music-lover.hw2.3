56fa08980be41028411f596a2238c6d718e3fb76
Timur Zaripov <zaripov_t99@mail.ru>
2021-02-12 23:57:05 +0700

    Edit.v2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -10,39 +10,22 @@ fun main() {
     while (true) {
         println("Введи сумму покупки или напиши -1 для выхода")
         var buy = scanner.nextDouble()
+        
         if (regularClient == true) {
             sale = buy * 0.01
         }
         if (buy == -1.0) {
-
             break
-        } else if (lastBuy <= 1000.0) {
-
-            lastBuy = buy - sale
-            println(
-                "Скидки нет\n" +
-                        "Сумма покупки составила: $lastBuy руб"
-            )
-        } else if ((lastBuy > 1000.0) && (lastBuy <= 10000.0)) {
-
-            lastBuy = (buy - 100.0) - sale
-            println(
-                "Скидка 100руб\n" +
-                        "Сумма покупки составила: $lastBuy руб"
-            )
-        } else if (lastBuy > 10000.0) {
-
-            lastBuy = (buy - buy * 0.05) - sale
-            println(
-                "Скидка 5% от суммы покупки\n" +
-                        "Сумма покупки составила: $lastBuy"
-            )
         }
 
-        if (buy != 0.0) {
-            regularClient = true
-        } else {
-            regularClient = false
+        when (lastBuy) {
+            in 0.0..1000.0 -> lastBuy = buy - sale
+            in 1001.0..10000.0 -> lastBuy = (buy - 100.0) - sale
+            else -> lastBuy = (buy - buy * 0.05) - sale
         }
+
+        println("Сумма покупки $lastBuy руб")
+
+        regularClient = buy != 0.0
     }
 }
